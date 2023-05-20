7452fae388721e9e14a2b13461461d006a50da5b
Vlad <vladsevelev08@gmail.com>
2023-05-20 17:55:28 +0300

    release 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from pyTelegramBotAPI import telebot
+from pytube import YouTube
 
+bot = telebot.TeleBot(token="6236996276:AAGtndPgpHa6c9wXy8hzXSqVoPBr0Wkjtbw")
+
+
+@bot.message_handler(commands=["start", "help"])
+
+def youtube_download(message):
+    yt = YouTube(message)
+    file = yt.streams.first().download()
+    
+@bot.message_handler(func=lambda message: True)
+def echo_call(message):
+    bot.reply_to(message, text=message.text)
+
+
+bot.infinity_polling()
